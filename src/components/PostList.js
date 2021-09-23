0e64cb3b3e05663b56a48f1043b52b8253df9c90
Heneros <punk60826@gmail.com>
2021-09-24 02:56:03 +0300

    created action

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -1,9 +1,14 @@
 import React  from "react";
+import { connect } from 'react-redux';
+import { fetchPosts } from '../actions';
 
 class PostList extends React.Component{
+    componentDidMount(){
+        this.props.fetchPosts();
+    }
     render(){
         return <h1>Post list</h1>;
     }
 }
 
-export default PostList;
+export default connect(null, {fetchPosts})(PostList);
